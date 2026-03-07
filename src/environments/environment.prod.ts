5346abf52505138750ecaca2df28e29d12c3e099
danweb-admin <143641802+danweb-admin@users.noreply.github.com>
2026-03-07 16:43:08 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    URL_API: 'https://backend2.rcc-londrina.online',
+    URL_API: 'https://backend.rcc-londrina.online',
     // URL_API: 'http://161.35.255.131:31354',
 };
